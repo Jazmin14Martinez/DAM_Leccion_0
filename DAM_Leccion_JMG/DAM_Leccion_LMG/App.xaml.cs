dd95db94404936cf8698b6ea37c12c2f5799cdb7
Jazmin14Martinez <20030463@itesa.edu.mx>
2025-01-28 17:52:53 -0600

    Version_01: Se creo el proyecto MAUI vacio.

---
+++ DAM_Leccion_JMG/DAM_Leccion_LMG/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DAM_Leccion_LMG
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
